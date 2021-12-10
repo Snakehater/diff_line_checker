1772052b59704cba51848cfcb653464c253bafcb
snakehater <vigor.turujlija@gmail.com>
2021-12-10 18:37:04 +0100

    made it look nicer

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include "main.hpp"
 
+// macros
+std::vector<std::string> split(std::string s, std::string delimiter, int splits=-1);
+
+
+
 std::string f1_path = "res/working.txt";
 std::string f2_path = "res/expired.txt";
 
 int main(){
+	// print info
+	std::vector<std::string> splitted_f1 = split(f1_path, "/");
+	std::vector<std::string> splitted_f2 = split(f2_path, "/");
+	std::cout << "Comparing " << splitted_f1[splitted_f1.size() -1];
+	std::cout << " and " << splitted_f2[splitted_f2.size() -1] << '.';
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "Main file is " << BOLDWHITE << splitted_f1[splitted_f1.size() -1] << RESET;
+	std::cout << " which means it will be displayed before the second file";
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+
 	std::ifstream f1(f1_path);
 	std::ifstream f2(f2_path);
 
@@ -14,10 +33,16 @@ int main(){
 
 	std::string f1_str;
 	std::string f2_str;
+
+	int row = 0;
 	while (std::getline(f1_stream, f1_str, '\n') &&
 		std::getline(f2_stream, f2_str, '\n')) {
-		std::string f1LineOut = "";
-		std::string f2LineOut = "";
+		row++;
+		std::string leftBuf = "";
+		for (unsigned long int i = 0; i < 3 - std::to_string(row).length(); i++)
+			leftBuf += ' ';
+		std::string f1LineOut = leftBuf + std::to_string(row) + " |";
+		std::string f2LineOut = leftBuf + std::to_string(row) + " |";
 		unsigned long int i = 0;
 		for (i = 0; i < f1_str.length() && i < f2_str.length(); i++) {
 			if (f1_str[i] == f2_str[i]) {
@@ -53,4 +78,23 @@ int main(){
 		std::cout << f1_str << std::endl;
 	while (std::getline(f2_stream, f2_str, '\n'))
 		std::cout << f2_str << std::endl;
+}
+
+
+
+std::vector<std::string> split(std::string s, std::string delimiter, int splits){
+	std::vector<std::string> output;
+	
+	size_t pos = 0;	
+	int i = 0;
+	std::string token;
+	while ((pos = s.find(delimiter)) != std::string::npos && i != splits) {
+		token = s.substr(0, pos);
+		output.push_back(token);
+		s.erase(0, pos + delimiter.length());
+		i++;
+	}
+	// add the rest of the string to last element
+	output.push_back(s);
+	return output;
 }
